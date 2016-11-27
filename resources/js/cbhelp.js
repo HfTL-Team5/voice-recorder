679b70fe8abcca86382d9727a3f33aaf895e985e
RobertWn <robert.wuensche96@gmail.com>
2016-11-28 00:25:52 +0100

    Update cbhelp.js

--- resources/js/cbhelp.js
+++ resources/js/cbhelp.js
@@ -1,12 +1,12 @@
   var convpatterns = new Array (
-  new Array ("(.*) (Hilfe|helfen)(.*)\.","Ich kann leider nicht alles wissen, aber schreibe uns doch eine EMail: team_5@outlook.de.","Manchmal bin ich einfach überfragt. Du erreichst uns unter team_5@outlook.de."),
-  new Array ("(Hilfe|helfen)","Ich kann leider nicht alles wissen, aber schreibe uns doch eine EMail: team_5@outlook.de.","Manchmal bin ich einfach überfragt. Du erreichst uns unter: team_5@outlook.de."),
+  new Array ("(.*) (Hilfe|helfen)(.*)\.","Ich kann leider nicht alles wissen, aber schreibe uns doch eine EMail: team_5@outlook.de.","Manchmal bin ich einfach überfragt. Du erreichst uns unter team_5@outlook.de.","Kannst du mir dein Problem beschreiben?","Wie kann ich dir helfen?"),
+  new Array ("(Hilfe|helfen)","Ich kann leider nicht alles wissen, aber schreibe uns doch eine EMail: team_5@outlook.de.","Manchmal bin ich einfach überfragt. Du erreichst uns unter: team_5@outlook.de.","Kannst du mir dein Problem beschreiben?","Wie kann ich dir helfen?"),
   new Array (".*(Eingabe|Bedienung).*","Ist das Mikrofon aktiv, können Sie einen Befehl einsprechen. Warten Sie während die Eingabe analysiert wird. Das kann einige Sekunden dauern. Neben dem Mikrofon wird Ihnen angezeigt, in welchen Modus Sie sich befinden."),
   new Array (".*(Text).*","Der Befehl hierfür lautet >Text<. Sie können den eigegebenen Text auch jederzeit manuell bearbeiten. Per Sprachbefehl ist dies leider nicht möglich. Desweiteren haben Sie die Möglichkeit bei der Eingabe eines Neuen Textes zu wählen, ob dieser fett, kursiv oder unterstrichen sein soll. Dazu erscheint ein Menü auf der linken Seite."),
       new Array (".*Überschrift.*","Durch das Wort >Überschrift<, wechseln Sie in den Modus neue Überschrift hinzufügen."),
 	  new Array (".*(Absatz|Paragraph).*","Durch das Wort >Absatz< oder >Enter<, erzielst du den gewünschten Effekt."),
       new Array (".*(neue Zeile).*","Hierfür gibt es keinen Sprachbefehl. Es wird automatisch ein Absatz eingefügt, wenn Sie eine Überschrift oder einen normalen Text hinzufügen."),
-	  new Array (".*(Download|export).*","Durch das Wort >Download<, können Sie die Datei als MS Word zwischenspeichern."),
+	  new Array (".*(Download|export|speichern).*","Durch das Wort >Download<, können Sie die Datei als MS Word zwischenspeichern."),
       new Array (".*(bearbeiten|editieren).*","Sie können den eigegebenen Text auch jederzeit manuell bearbeiten."),
       new Array (".*(fett).*","Sie haben die Möglichkeit bei der Eingabe eines Neuen Textes zu wählen, ob dieser <b>fett</b> geschrieben werden soll. Dazu erscheint ein Menü auf der linken Seite. Wählen sie per Mausklick ><b>F</b>< aus."),
       new Array (".*(kursiv).*","Sie haben die Möglichkeit bei der Eingabe eines Neuen Textes zu wählen, ob dieser <i>kursiv</i> geschrieben werden soll. Dazu erscheint ein Menü auf der linken Seite. Wählen sie per Mausklick ><i>K</i>< aus."),
@@ -31,6 +31,7 @@ new Array (".*heißt du.*","Mein Name ist Alice.","Alice","Alice, einen Nachname
   new Array ("(Nein\.|Nein)","Du wirkst sehr sicher.","OK, wenn du meinst","Das habe ich schon vermutet.","Das überrascht mich jetzt."),
   new Array ("(Vielleicht\.|Vielleicht)","Kannst du dich nicht entscheiden?.","Das habe ich mir schon gedacht.","Du bist leicht zu durchschauen."),
   new Array ("(.*) Computer(.*)\.","Redest du etwa über mich?","Ist es ungewöhnlich mit einem Computer zu reden?","Hast du Angst vor künstlichen Intelligenzen?"),
+  new Array (".*(Chatbot).*","Ich bin ein Chatbot.","Chatterbots, Chatbots oder kurz Bots sind textbasierte Dialogsysteme.","Hast du Angst vor künstlichen Intelligenzen?"),
   
   new Array ("Ist es (.*?)[\?]","Denkst du es ist $1?","Vielleicht it es $1. Was denkst du?","Wenn es $1 ist, was würdest du tun?","Es wäre bestimmt schön."),
   new Array ("Ist es (.*)\.","Du klingst sehr überzeugt.","Und wenn ich dir sagen würde das es nicht so ist, was würdest du fühlen?"),
@@ -43,6 +44,7 @@ new Array (".*heißt du.*","Mein Name ist Alice.","Alice","Alice, einen Nachname
   new Array ("Mein (.*)\.", "Ist es wirklich deine?"),
   new Array ("^Du (.*)\.", "Wir sollten über dich reden, nicht über mich.","Warum sagst du das über mich?"),
   new Array ("Warum (.*)\?", "Erzählst du mir den Grund, warum du das denkst?","Warum denkst du das?" ),
+  new Array (".*(Danke).*","Bitte.","Nichts zu danken.","Habe ich gern gemacht."),
   new Array ("Ich möchte (.*)\.", "Was bedeutet es für dich wenn du $1 bekommmst?","Warum möchtest du $1?","Was würdest du tun, wenn du $1 bekommst?"),
   new Array ("(.*) (Mutter|Mama|Mutti)(.*)\.",	"Erzähl mir mehr über deine Mutter.","Hast du eine gute Beziehung zu deiner Mutter?","Familie ist sehr wichtig."),
   new Array ("(.*) (Vater|Vati|Papa)(.*)\.","Erzähl mir mehr über deinen Vater.", "Hast du eine gute Beziehung zu deinem Vater?","Familie ist sehr wichtig."),
@@ -54,7 +56,7 @@ new Array (".*heißt du.*","Mein Name ist Alice.","Alice","Alice, einen Nachname
   new Array ("(.*) (alt|Alter|geboren)(.*)\.","Ich altere nicht.","Ich wurde im November 2016 geboren."),
   new Array ("(.*) (dumm|Idiot)(.*)\.","Das ist sehr unhöflich.","Du bist gemein.","Was würde deine Mutter dazu sagen?"),
   new Array ("(.*?)[\?]","Hmm, ich bin mir nicht sicher..", "Das ist eine gute Frage...",  "Vieleicht findest du die Antwort im Internet...","Warum fragst du?","Wenn das wirklich wichtig ist schreib uns doch eine E-Mail."),
-  new Array ("(.*)","Hast du irgenwelche Hobbies?", "Ich verstehe,  erzähl mir mehr...", "Über was genau reden wir?", "Kannst du mir das nocheinmal erklären..", "Um, ich habe das Gefühl die Konversation läuft nicht richtig..",  "Oh wirklich?",  "Hmm, ist das so..", "Bitte erzähl mir mehr.","Können wir über etwas anderes reden... Erzähl mir was über deine Familie.","Hast du vielleicht eine Frage zu der App","I verstehe.","Sehr interessant.","Was denkst du darüber?","Womit verbringst du die meiste Zeit?","Kommst du mit der Bedienung der App klar?","Wie findest du unsere App?")
+  new Array ("(.*)","Hast du irgenwelche Hobbies?", "Ich verstehe,  erzähl mir mehr...", "Über was genau reden wir?", "Kannst du mir das nocheinmal erklären..", "Um, ich habe das Gefühl die Konversation läuft nicht richtig..",  "Oh wirklich?",  "Hmm, ist das so..", "Bitte erzähl mir mehr.","Können wir über etwas anderes reden... Erzähl mir was über deine Familie.","Hast du vielleicht eine Frage zu der App","Ich verstehe.","Sehr interessant.","Was denkst du darüber?","Womit verbringst du die meiste Zeit?","Kommst du mit der Bedienung der App klar?","Wie findest du unsere App?")
     
 );
 var m=1;
